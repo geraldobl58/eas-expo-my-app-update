c6f582ee5748310de55287be811ce13ba5e19cc2
Geraldo Luiz <geraldobl58@gmail.com>
2023-01-16 18:45:08 -0300

    chore: Add github actions

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My app with EAS Upadate deploy</Text>
+      <Text>My app with EAS Update deploy</Text>
+      <Text>Deploy with github actions</Text>
       <StatusBar style="auto" />
     </View>
   );
